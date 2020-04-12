f295fc0cf5fa9858022ac850c30bd1a42f4a0f53
Thanh Quy <thanhquylethi@gmail.com>
2020-04-13 00:04:16 +0700

    Add quadratic-equation php file

---
+++ quadratic-equation/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quadratic equation</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+<div class="col-md-5">
+    <form class="navbar-form navbar-left" role="search" method="post" action="view.php">
+        <h3><b>Giải phương trình bậc 2</b></h3>
+        <br>
+        <span style="font-size: 16px">Nhập số a:</span>
+        <input type="text" class="form-control" name="a" >
+        <br>
+        <br>
+        <span style="font-size: 16px">Nhập số b:</span>
+        <input type="text" class="form-control" name="b" >
+        <br>
+        <br>
+        <span style="font-size: 16px">Nhập số c:</span>
+        <input type="text" class="form-control" name="c" >
+        <br>
+        <br>
+        <button type="submit" class="btn btn-success">Giải phương trình</button>
+
+    </form>
+</div>
+</body>
+</html>
